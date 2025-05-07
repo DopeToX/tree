5d51d612c45937061c23438779756ac130cf252d
eshkere monstr <154499029+DopeToX@users.noreply.github.com>
2025-05-07 08:18:54 +0000

    working on tree

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+class tree{
+    Node(Node)* root;
+}
+class Node
+{
+public:
+    int data;
+    Node* left, * right;
 
+    Node(int key){
+        data = key;
+        left=0;
+        right=0;
+    }
+
+}
